4a85945c2c2640ee46d03e48a08b4d7c3808925d
Nishit <nishitsuthar123@gmail.com>
2023-08-02 14:20:47 +0530

    first commit adding to main

---
+++ force-app/main/default/aura/NewQuoteItemClone/NewQuoteItemCloneRenderer.js
@@ -0,0 +1,10 @@
+({
+    afterRender: function (component, helper) {
+        this.superAfterRender();
+        /*cmp.find("inputId").focus();*/
+        // console.log('inside after render');
+        // var mainDiv = component.find("av").getElement();
+        // var Element = document.getElementById("slds");
+        // console.log('bottom => ', Element.clientHeight);
+    }
+})
