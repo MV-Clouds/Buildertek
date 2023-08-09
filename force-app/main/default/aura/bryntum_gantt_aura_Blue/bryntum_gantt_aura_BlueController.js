d7524019ab98db7f1069cd034ca625a459737a3e
nnirbhay <naviknirbhay@gmail.com>
2023-08-09 11:08:40 +0530

    latest changes to be merge with master

---
+++ force-app/main/default/aura/bryntum_gantt_aura_Blue/bryntum_gantt_aura_BlueController.js
@@ -0,0 +1,23 @@
+({
+    doInit: function(component, event) {
+
+        //projName
+
+    },
+    handleFilterChange: function(component, event) {
+
+    },
+    handleRefreshFromTable :  function(component, event) {
+
+    },
+
+    /*onTabClosed  :  function(component, event) {
+        component.set("v.tabClosedAtt",true);
+
+       // component.destroy()
+    },*/
+     onTabFocused : function(component, event, helper) {
+        
+    }
+
+})
