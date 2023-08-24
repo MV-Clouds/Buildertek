639b08a982970eebc26e2bb682518c17e69f402d
MV Clouds <mvclouds@MVs-MacBook-Air.local>
2023-08-24 16:59:16 -0700

    BUG Fix Close Icon BUIL-3456

--- force-app/main/default/aura/BT_EmailCO/BT_EmailCOController.js
+++ force-app/main/default/aura/BT_EmailCO/BT_EmailCOController.js
@@ -244,8 +244,21 @@
         }
     },                
     closeWindow: function(component, event, helper) {
+            var NewSignBox = component.get("c.NewSignChecked");
+            NewSignBox.setParams({
+                recordId : component.get("v.recordId"),
+            });
+        $A.enqueueAction(NewSignBox);
         $A.get("e.force:closeQuickAction").fire();
-    }
+    },
+    handleDestroy : function (component, event, helper) {
+        var NewSignBox = component.get("c.NewSignChecked");
+            NewSignBox.setParams({
+                recordId : component.get("v.recordId"),
+            });
+        $A.enqueueAction(NewSignBox);
+        $A.get("e.force:closeQuickAction").fire();
+      }       
 
               
                 
