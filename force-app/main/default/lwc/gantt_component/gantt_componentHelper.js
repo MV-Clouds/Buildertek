3f20e8c1d2868747cffcff242f11c38742bab057
Karan3036 <karsingh9377@gmail.com>
2023-08-29 11:22:24 +0530

    BT-998 <Duration issue>

--- force-app/main/default/lwc/gantt_component/gantt_componentHelper.js
+++ force-app/main/default/lwc/gantt_component/gantt_componentHelper.js
@@ -777,7 +777,7 @@ function calcBusinessDays(dDate1, dDate2) { // input given as Date objects
     // calculate differnece in weeks (1000mS * 60sec * 60min * 24hrs * 7 days = 604800000)
     iWeeks = Math.floor((dDate2.getTime() - dDate1.getTime()) / 604800000)
 
-    if (iWeekday1 < iWeekday2) { //Equal to makes it reduce 5 days
+    if (iWeekday1 <= iWeekday2) { //Change '<' to '<=' to include the same date
       iDateDiff = (iWeeks * 5) + (iWeekday2 - iWeekday1)
     } else {
       iDateDiff = ((iWeeks + 1) * 5) - (iWeekday1 - iWeekday2)
