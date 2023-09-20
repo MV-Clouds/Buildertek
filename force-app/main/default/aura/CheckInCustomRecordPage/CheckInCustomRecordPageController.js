0dafd2f5d833107ee937f75ed5bbb6e0dcb8aa49
nnirbhay <naviknirbhay@gmail.com>
2023-09-20 16:30:20 +0530

    implement new image preview on checkincustomRecordPage cmp

--- force-app/main/default/aura/CheckInCustomRecordPage/CheckInCustomRecordPageController.js
+++ force-app/main/default/aura/CheckInCustomRecordPage/CheckInCustomRecordPageController.js
@@ -37,4 +37,37 @@
         // helper.handleCreateCheckIn(component, next, prev);
     },
 
+
+    //  New Image Preview --------------
+    openCustomPreview: function(component, event, helper){
+        var imageSrc = event.getSource().get("v.src");
+        component.set("v.PreviewImageSrc", imageSrc);
+        var imageTitle = event.getSource().get("v.description");
+        component.set("v.PreviewImageTitle", imageTitle);
+        console.log('description >> ', event.getSource().get("v.description"));
+
+       const img_preview = component.find("img_preview").getElement();
+       img_preview.style = 'display : flex; background: rgba(0, 0, 0, 0.8);'
+    },
+
+    closeImagePreview : function(component, event, helper){
+        const img_preview = component.find("img_preview").getElement();
+       img_preview.style = '';
+    },
+
+    downloadImage: function(component, event, helper){
+        var a = document.createElement('a');
+        a.href = component.get("v.PreviewImageSrc");
+        a.download = component.get("v.PreviewImageSrc");
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+    },
+
+    stopEventPropogation: function(component, event, helper){
+        event.stopPropagation();
+    }
+
+    //  New Image Preview  END --------------------
+
 })
