e5dd2e7a8881985de73de09f987bd222a2314449
MV Clouds <mvclouds@MVs-MacBook-Air-3.local>
2023-10-18 12:17:05 +0530

    BT-1105 BUIL-3594 popup update

--- force-app/main/default/aura/BT_Sync_Product_Redirect/BT_Sync_Product_RedirectController.js
+++ force-app/main/default/aura/BT_Sync_Product_Redirect/BT_Sync_Product_RedirectController.js
@@ -15,7 +15,7 @@
                     toastEvent.setParams({
                         "title": "All Quote Line have Products",
                         "type": "error",
-                        "message": "All Quote Line have been sync'd to a product."
+                        "message": "All Quote Line have been sycgit n'd to a product."
 
                     });
                     toastEvent.fire();
