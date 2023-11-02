5b819242763254d3d90e3d2a01cebecf3430d05b
Karan3036 <karsingh9377@gmail.com>
2023-11-02 15:05:34 +0530

    BT-1153 <Done.>

--- force-app/main/default/aura/ScheduleSummary/ScheduleSummaryController.js
+++ force-app/main/default/aura/ScheduleSummary/ScheduleSummaryController.js
@@ -2,5 +2,63 @@
     doInit: function (component, event, helper) {
         var recordId = component.get("v.recordId");
         helper.loadTasks(component, recordId);
+        helper.getContractorListHelper(component, event, helper);
+    },
+    filterRec: function(component, event,helper) {
+        var contractorId = component.get("v.selectedContractorId");
+        var recordId = component.get("v.recordId");
+        helper.FieterTaskWithVendorNameAndCurrentProject(component, recordId, contractorId);
+    },
+    searchContractorData : function(component, event, helper) {
+        component.set('v.displayContractor', true);
+        event.stopPropagation();
+    },
+    keyupContractorData: function (component, event, helper) {
+        component.set('v.displayContractor', true);
+        var searchFilter = event.getSource().get("v.value").toUpperCase();
+        setTimeout($A.getCallback(function() {
+            var listOfAllRecords = component.get('v.allContractorRecords');
+            var tempArray = [];
+            var i;
+            
+            for (i = 0; i < listOfAllRecords.length; i++) {
+                var recordName = listOfAllRecords[i].Name;
+                if (recordName && recordName.toUpperCase().indexOf(searchFilter) !== -1) {
+                    tempArray.push(listOfAllRecords[i]);
+                }
+            }
+            
+            if (tempArray.length === 0) {
+                component.set('v.selectedContractorId', ' ');
+                component.set("v.contractorList", []);
+                component.set("v.Message", 'There are no Contractors.');
+                // component.set("v.Spinner", false);
+                
+            } else {
+                component.set("v.contractorList", tempArray);
+            }
+        }), 2000);
+    },
+    hideList : function(component, event, helper) {
+        component.set('v.displayContractor', false);
+    },
+    clearInput: function(component, event, helper) {
+        component.set('v.selectedContractorName','');
+        component.set('v.selectedContractorId','');
+    },
+    preventHide: function(component, event, helper) {
+        event.preventDefault();
+    },
+    clickHandlerContractor: function(component, event, helper){
+        component.set('v.displayContractor', false);
+        var recordId = event.currentTarget.dataset.value;
+        component.set('v.selectedContractorId', recordId);
+        var contractorList = component.get("v.contractorList");
+        contractorList.forEach(element => {
+            if (recordId == element.Id) {
+                component.set('v.selectedContractorName', element.Name);
+
+            }
+        });
     },
 })
--- force-app/main/default/aura/ScheduleSummary/ScheduleSummaryHelper.js
+++ force-app/main/default/aura/ScheduleSummary/ScheduleSummaryHelper.js
@@ -1,55 +1,132 @@
 ({
     loadTasks: function (component, recordId) {
         var action = component.get("c.gettaskOfSchedules");
-        action.setParams({ "recordId": recordId });
-
+        action.setParams({
+            "recordId": recordId
+        });
         action.setCallback(this, function (response) {
             var state = response.getState();
+
             if (state === "SUCCESS") {
                 var tasks = response.getReturnValue();
                 console.log('tasks---->', tasks);
+                if (tasks.length > 0) {
+                    tasks.forEach(task => {
+                        if (!task.buildertek__Contractor__c) {
+                            task.buildertek__Contractor__c = '';
+                        }else{
+                            task.buildertek__Contractor__c = task.buildertek__Contractor__c;
+                        }
+                    });
+                }
+
                 component.set("v.tasks", tasks);
+                component.set("v.taskListInit", tasks);
                 this.filterTasks(component);
+
             } else {
-                console.log("Error: " + response.getError());
+                $A.get("e.force:closeQuickAction").fire() 
+                        var toastEvent = $A.get("e.force:showToast");
+                        toastEvent.setParams({
+                            "title": "Error",
+                            "type": "error",
+                            "message": "Something went wrong."  
+                        });
+                toastEvent.fire();
             }
         });
-
         $A.enqueueAction(action);
     },
-
     filterTasks: function (component) {
-        try {
-            var tasks = component.get("v.tasks");
-            var today = new Date();
-            var thisWeekStart = new Date(today);
-            thisWeekStart.setDate(today.getDate() - today.getDay()); // Start of the week (Sunday)
-            var thisWeekEnd = new Date(thisWeekStart);
-            thisWeekEnd.setDate(thisWeekStart.getDate() + 6); // End of the week (Saturday)
+        var taskList = component.get("v.tasks");
+        var tasks= [];
 
-            // Implement the logic to filter tasks into Past Due, Due This Week, and Due Next Week
-            var pastDueTasks = tasks.filter(task => new Date(task.buildertek__Finish__c) < today);
-            var dueThisWeekTasks = tasks.filter(task => new Date(task.buildertek__Finish__c) >= thisWeekStart && new Date(task.buildertek__Finish__c) <= thisWeekEnd);
-            var nextWeekStart = new Date(thisWeekEnd);
-            nextWeekStart.setDate(thisWeekEnd.getDate() + 1); // Start of next week (Sunday)
-            console.log('nextWeekStart--->',nextWeekStart);
-            var nextWeekEnd = new Date(nextWeekStart);
-            nextWeekEnd.setDate(nextWeekStart.getDate() + 6); // End of next week (Saturday)
-            console.log('nextWeekEnd--->',nextWeekEnd);
-            var finish = new Date(tasks[0].buildertek__Finish__c);
-            console.log('finishdate--->',finish);
-            var dueNextWeekTasks = tasks.filter(task => new Date(task.buildertek__Finish__c) >= nextWeekStart && new Date(task.buildertek__Finish__c) <= nextWeekEnd);
-            if (new Date(tasks[0].buildertek__Finish__c) == nextWeekStart) {
-                alert("Hello");
-            } else {
-                alert("Bye");
+        taskList.forEach(function(task) {
+            if (task) {
+                var dateStr = task.buildertek__Finish__c;
+                if (dateStr) {
+                    var date = new Date(dateStr);
+                    var month = (date.getMonth() + 1).toString().padStart(2, '0');
+                    var day = date.getDate().toString().padStart(2, '0');
+                    var year = date.getFullYear().toString().substr(-2);
+                    dateStr = month + '-' + day + '-' + year;
+                }
+                task.buildertek__Finish__c = dateStr;
+                tasks.push(task);
             }
+        });
+        
+        var today = new Date();
+        today.setHours(0, 0, 0, 0); // Set the time to midnight
 
-            component.set("v.pastDueTasks", pastDueTasks);
-            component.set("v.dueThisWeekTasks", dueThisWeekTasks);
-            component.set("v.dueNextWeekTasks", dueNextWeekTasks);
-        } catch (error) {
-            console.log('error--->',error);
+        var thisWeekStart = new Date(today);
+        thisWeekStart.setDate(today.getDate() - today.getDay()); // Start of the week (Sunday)
+        thisWeekStart.setHours(0, 0, 0, 0); // Set the time to midnight
+
+        var thisWeekEnd = new Date(thisWeekStart);
+        thisWeekEnd.setDate(thisWeekStart.getDate() + 6); // End of the week (Saturday)
+        thisWeekEnd.setHours(23, 59, 59, 999); // Set the time to 23:59:59.999
+
+        // Implement the logic to filter tasks into Past Due, Due This Week, and Due Next Week
+        var pastDueTasks = tasks.filter(task => new Date(task.buildertek__Finish__c) < thisWeekStart);
+        var dueThisWeekTasks = tasks.filter(task => {
+            var taskDate = new Date(task.buildertek__Finish__c);
+            return taskDate >= thisWeekStart && taskDate <= thisWeekEnd;
+        });
+
+        var nextWeekStart = new Date(thisWeekEnd);
+        nextWeekStart.setDate(thisWeekEnd.getDate() + 1); // Start of next week (Sunday)
+        nextWeekStart.setHours(0, 0, 0, 0); // Set the time to midnight
+
+        var nextWeekEnd = new Date(nextWeekStart);
+        nextWeekEnd.setDate(nextWeekStart.getDate() + 6); // End of next week (Saturday)
+        nextWeekEnd.setHours(23, 59, 59, 999); // Set the time to 23:59:59.999
+
+        var dueNextWeekTasks = tasks.filter(task => {
+            var taskDate = new Date(task.buildertek__Finish__c);
+            return taskDate >= nextWeekStart && taskDate <= nextWeekEnd;
+        });
+
+        component.set("v.pastDueTasks", pastDueTasks);
+        component.set("v.dueThisWeekTasks", dueThisWeekTasks);
+        component.set("v.dueNextWeekTasks", dueNextWeekTasks);
+    },
+    FieterTaskWithVendorNameAndCurrentProject: function(component,recordId, contractorId) {
+        var taskList = component.get("v.tasks");
+        if (contractorId) {
+            var matchingTasksList = [];
+            taskList.filter(function(task) {
+                if(task.buildertek__Contractor__c === contractorId){
+                    matchingTasksList.push(task);
+                }
+            });
+            // Set the matching tasks list back to the original taskList
+            component.set("v.tasks", matchingTasksList);
+            this.filterTasks(component);        
+        } else {
+            var taskListInit = component.get("v.taskListInit");
+            component.set("v.tasks", taskListInit);
+            this.filterTasks(component);
+
         }
-    }
+    },
+    getContractorListHelper: function(component, event, helper) {
+
+        var action = component.get("c.getContractorRecords");
+        component.set("v.Message" , 'Loading...');
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            var result= response.getReturnValue();
+            if (state === "SUCCESS") {
+                component.set('v.contractorList' ,result);
+                if (result.length > 0) {
+                    component.set("v.Message" , '');  
+                }else{
+                    component.set("v.Message" , 'There are No Contractor.');  
+                }
+                component.set('v.allContractorRecords' ,result);
+            }
+        });
+        $A.enqueueAction(action);
+    },
 })
