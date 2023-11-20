c12eeb17ac266e7100291aec2293e070b033b5dc
patelkaran0104 <khpatel0104@gmail.com>
2023-11-20 19:16:12 +0530

    import master schedule changes

--- force-app/main/default/aura/ImportMasterSchedule/ImportMasterScheduleController.js
+++ force-app/main/default/aura/ImportMasterSchedule/ImportMasterScheduleController.js
@@ -8,12 +8,12 @@
         component.set("v.BaseURLs", baseURL);
         var myPageRef = component.get("v.pageReference");
         var recordId;
-        if(myPageRef){
-             recordId = myPageRef.state.buildertek__RecordId;
+        if (myPageRef) {
+            recordId = myPageRef.state.buildertek__RecordId;
         }
-        
-        if(recordId){
-            component.set("v.RecordId",recordId)
+
+        if (recordId) {
+            component.set("v.RecordId", recordId)
             component.set("v.isNewGantt", myPageRef.state.buildertek__isFromNewGantt)
             //recordId = component.get("v.RecordId");
             /*if(!recordId){
@@ -21,7 +21,7 @@
             }*/
         }
         var action = component.get("c.getMasterSchedule");
-       
+
         action.setParams({//recordIdFromUrl
             recordId: component.get("v.RecordId")
         });
@@ -104,15 +104,15 @@
                         component.find("checkContractor").set("v.value", true);
                         var checkbox = component.find("checkContractor").get("v.text");
                         Submittals[i].ScheduleCheck = true;
-                        
-                        
+
+
                     } else {
                         component.find("checkContractor").set("v.value", false);
-                        
+
                         var checkbox = component.find("checkContractor").get("v.text");
                         var Submittals = component.get("v.masterSchedulesList");
                         Submittals[i].ScheduleCheck = false;
-                        
+
                     }
                 }
             }
@@ -125,114 +125,195 @@
         var url = location.href;
         var baseURL = url.substring(0, url.indexOf('--', 0));
         var recordId = component.get("v.RecordId");
-        if(component.get('v.isNewGantt')){
+        if (component.get('v.isNewGantt')) {
             //window.open(baseURL + '.lightning.force.com/lightning/r/buildertek__Schedule__c/' + escape(recordId) + '/view', '_top');
             var workspaceAPI = component.find("workspace");
-            if(workspaceAPI.getFocusedTabInfo()){
-                workspaceAPI.getFocusedTabInfo().then(function(response) {
+            if (workspaceAPI.getFocusedTabInfo()) {
+                workspaceAPI.getFocusedTabInfo().then(function (response) {
                     var focusedTabId = response.tabId;
-                    workspaceAPI.closeTab({tabId: focusedTabId});
+                    workspaceAPI.closeTab({ tabId: focusedTabId });
                 })
-                .catch(function(error) {
-                    console.log(error);
-                    var navEvt = $A.get("e.force:navigateToSObject");
-                                    navEvt.setParams({
-                                        "recordId": recordId,
-                                        "slideDevName": "detail"
-                                    });
-                                    navEvt.fire();
-                    //window.open(baseURL + '.lightning.force.com/lightning/r/buildertek__Schedule__c/' + escape(recordId) + '/view', '_top');
-                });
-            }else{
+                    .catch(function (error) {
+                        console.log(error);
+                        var navEvt = $A.get("e.force:navigateToSObject");
+                        navEvt.setParams({
+                            "recordId": recordId,
+                            "slideDevName": "detail"
+                        });
+                        navEvt.fire();
+                        //window.open(baseURL + '.lightning.force.com/lightning/r/buildertek__Schedule__c/' + escape(recordId) + '/view', '_top');
+                    });
+            } else {
                 window.open(baseURL + '.lightning.force.com/lightning/r/buildertek__Schedule__c/' + escape(recordId) + '/view', '_top');
             }
-            
-        }else{
+
+        } else {
             window.open('/apex/BT_Task_Manager?recordId=' + escape(recordId), '_self')
         }
-        
+
         //window.open(baseURL + '.lightning.force.com/lightning/r/buildertek__Schedule__c/' + escape(recordId) + '/view', '_self');
     },
 
+    // importSchedule: function (component, event, helper) {
+    //     component.set("v.Spinner", true);
+    //     component.set("v.showMessage", true);
+    //     component.set("v.isError", false);
+    //     var url = location.href;
+    //     var baseURL = url.substring(0, url.indexOf('--', 0));
+    //     var SchedulesList = component.get("v.masterSchedulesList");
+    //     var ScheduleIds = [];
+    //     for (var i = 0; i < SchedulesList.length; i++) {
+    //         if (SchedulesList[i].scheduleCheck == true) {
+    //             ScheduleIds.push(SchedulesList[i].masterscheduleRecord.Id);
+    //         }
+    //     }
+    //     if (ScheduleIds.length > 0) {
+    //         var action = component.get("c.importMasterScheduleLines");
+    //         action.setParams({
+    //             scheduleRecIds: ScheduleIds,
+    //             recordId: component.get("v.RecordId")
+    //         });
+    //         action.setCallback(this, function (response) {
+    //             var state = response.getState();
+    //             if (state === "SUCCESS") {
+    //                 var result = response.getReturnValue();
+    //                 console.log('result ===>', { result });
+    //                 if (result.Status === 'Success') {
+    //                     component.set("v.isSucess", true);
+    //                     component.set("v.SucessMessage", result.Message);
+    //                     component.set("v.Spinner", false);
+    //                     component.set("v.showMessage", false);
+    //                     var baseURL = component.get("v.BaseURLs");
+    //                     var recordId = component.get("v.RecordId");
+    //                     if (component.get('v.isNewGantt')) {
+    //                         //window.open(baseURL + '.lightning.force.com/lightning/r/buildertek__Schedule__c/' + escape(recordId) + '/view', '_top');
+    //                         var workspaceAPI = component.find("workspace");
+    //                         if (workspaceAPI.getFocusedTabInfo()) {
+    //                             workspaceAPI.getFocusedTabInfo().then(function (response) {
+    //                                 var focusedTabId = response.tabId;
+    //                                 workspaceAPI.closeTab({ tabId: focusedTabId }).then(function (res) {
+    //                                     /*workspaceAPI.getFocusedTabInfo().then(function(response) {
+    //                                         var focusedTabId = response.tabId;
+    //                                        // $A.get('e.force:refreshView').fire();
+    //                                         window.open('/'+recordId,'_top')
+    //                                     }).catch(function(error) {
+    //                                         console.log(error);
+    //                                     });*/
+    //                                     if (res) {
+    //                                         window.open('/' + recordId, '_top')
+    //                                     }
+
+    //                                     //$A.enqueueAction(component.get('c.doInit'))
+    //                                     // $A.enqueueAction(navEvt.fire());
+    //                                 }).catch(function (error) {
+    //                                     console.log(error);
+    //                                 });
+
+    //                             }).catch(function (error) {
+    //                                 console.log(error);
+    //                                 var navEvt = $A.get("e.force:navigateToSObject");
+    //                                 navEvt.setParams({
+    //                                     "recordId": recordId,
+    //                                     "slideDevName": "detail"
+    //                                 });
+
+    //                                 $A.enqueueAction(component.get('c.doInit'))
+    //                                 $A.enqueueAction(navEvt.fire());
+    //                                 location.reload();
+    //                             });
+    //                         } else {
+    //                             window.open('/' + recordId, "_top");
+    //                             // window.open(baseURL + '.lightning.force.com/lightning/r/buildertek__Schedule__c/' + escape(recordId) + '/view', '_top');
+    //                         }
+
+    //                     } else {
+    //                         window.open('/apex/BT_Task_Manager?recordId=' + escape(recordId), '_self')
+    //                     }
+    //                     //window.open('/apex/BT_Task_Manager?recordId=' + escape(recordId), '_self')
+    //                 } else {
+    //                     component.set("v.Spinner", false);
+    //                     component.set("v.showMessage", false);
+    //                     component.set("v.isError", true);
+    //                     component.set("v.ErrorMessage", 'There are no schedule lines for the selected schedule.');
+    //                 }
+    //             }
+    //         });
+    //         $A.enqueueAction(action);
+    //     } else {
+    //         component.set("v.Spinner", false);
+    //         component.set("v.isError", true);
+    //         component.set("v.ErrorMessage", 'Please select at least one schedule to import.');
+
+    //     }
+    // },
+    
     importSchedule: function (component, event, helper) {
         component.set("v.Spinner", true);
-        component.set("v.showMessage",true);
+        component.set("v.showMessage", true);
         component.set("v.isError", false);
-        var url = location.href;
-        var baseURL = url.substring(0, url.indexOf('--', 0));
         var SchedulesList = component.get("v.masterSchedulesList");
+        console.log('SchedulesList ===>', typeof(SchedulesList));
+        debugger;
         var ScheduleIds = [];
         for (var i = 0; i < SchedulesList.length; i++) {
             if (SchedulesList[i].scheduleCheck == true) {
                 ScheduleIds.push(SchedulesList[i].masterscheduleRecord.Id);
             }
         }
+        console.log('ScheduleIds ===>', { ScheduleIds });
         if (ScheduleIds.length > 0) {
-            var action = component.get("c.importMasterScheduleLines");
+            var action = component.get("c.createScheduleLineFromMasterSchedule");
             action.setParams({
-                scheduleRecIds: ScheduleIds,
-                recordId: component.get("v.RecordId")
+                recordId: component.get("v.RecordId"),
+                masterId: ScheduleIds
             });
             action.setCallback(this, function (response) {
                 var state = response.getState();
                 if (state === "SUCCESS") {
                     var result = response.getReturnValue();
-                    console.log('result ===>', {result});
+                    console.log('result ===>', { result });
                     if (result.Status === 'Success') {
                         component.set("v.isSucess", true);
                         component.set("v.SucessMessage", result.Message);
                         component.set("v.Spinner", false);
-                        component.set("v.showMessage",false);
-                        var baseURL = component.get("v.BaseURLs");
+                        component.set("v.showMessage", false);
+
                         var recordId = component.get("v.RecordId");
-                        if(component.get('v.isNewGantt')){
-                            //window.open(baseURL + '.lightning.force.com/lightning/r/buildertek__Schedule__c/' + escape(recordId) + '/view', '_top');
+                        if (component.get('v.isNewGantt')) {
                             var workspaceAPI = component.find("workspace");
-                            if(workspaceAPI.getFocusedTabInfo()){
-                                workspaceAPI.getFocusedTabInfo().then(function(response) {
+                            if (workspaceAPI.getFocusedTabInfo()) {
+                                workspaceAPI.getFocusedTabInfo().then(function (response) {
                                     var focusedTabId = response.tabId;
-                                    workspaceAPI.closeTab({tabId: focusedTabId}).then(function(res){
-                                        /*workspaceAPI.getFocusedTabInfo().then(function(response) {
-                                            var focusedTabId = response.tabId;
-                                           // $A.get('e.force:refreshView').fire();
-                                            window.open('/'+recordId,'_top')
-                                        }).catch(function(error) {
-                                            console.log(error);
-                                        });*/
-                                        if(res){
-                                            window.open('/'+recordId,'_top')
+                                    workspaceAPI.closeTab({ tabId: focusedTabId }).then(function (res) {
+                                        if (res) {
+                                            window.open('/' + recordId, '_top')
                                         }
-                                        
-                                        //$A.enqueueAction(component.get('c.doInit'))
-                                        // $A.enqueueAction(navEvt.fire());
-                                    }).catch(function(error) {
+                                    }).catch(function (error) {
                                         console.log(error);
                                     });
-                                    
-                                }).catch(function(error) {
+
+                                }).catch(function (error) {
                                     console.log(error);
                                     var navEvt = $A.get("e.force:navigateToSObject");
                                     navEvt.setParams({
                                         "recordId": recordId,
                                         "slideDevName": "detail"
                                     });
-                                    
+
                                     $A.enqueueAction(component.get('c.doInit'))
                                     $A.enqueueAction(navEvt.fire());
                                     location.reload();
                                 });
-                            }else{
-                                window.open('/'+recordId, "_top");
-                               // window.open(baseURL + '.lightning.force.com/lightning/r/buildertek__Schedule__c/' + escape(recordId) + '/view', '_top');
+                            } else {
+                                window.open('/' + recordId, "_top");
                             }
-                            
-                        }else{
+
+                        } else {
                             window.open('/apex/BT_Task_Manager?recordId=' + escape(recordId), '_self')
                         }
-                        //window.open('/apex/BT_Task_Manager?recordId=' + escape(recordId), '_self')
                     } else {
                         component.set("v.Spinner", false);
-                        component.set("v.showMessage",false);
+                        component.set("v.showMessage", false);
                         component.set("v.isError", true);
                         component.set("v.ErrorMessage", 'There are no schedule lines for the selected schedule.');
                     }
@@ -246,7 +327,6 @@
 
         }
     },
-
     next: function (component, event, helper) {
         component.set("v.isError", false);
         var sObjectList = component.get("v.masterSchedulesList");
@@ -289,5 +369,5 @@
         component.set("v.endPage", end);
         component.set('v.PaginationList', Paginationlist);
     },
-    
+
 })
