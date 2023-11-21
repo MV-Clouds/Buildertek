2bb97a02437ca4fc122b31236983626aec0ae563
patelkaran0104 <khpatel0104@gmail.com>
2023-11-21 12:26:45 +0530

    apex class fix

--- force-app/main/default/aura/ImportMasterSchedule/ImportMasterScheduleController.js
+++ force-app/main/default/aura/ImportMasterSchedule/ImportMasterScheduleController.js
@@ -49,76 +49,66 @@
         $A.enqueueAction(action);
     },
 
-    handleCheck: function (component, event, helper) {
+    handleRadioChange: function(component, event, helper) {
         component.set("v.isError", false);
-        var checkbox = event.getSource();
-        var Submittals = component.get("v.masterSchedulesList");
-        for (var i = 0; i < Submittals.length; i++) {
-
-            if (Submittals[i].masterscheduleRecord.Id == checkbox.get("v.text") && Submittals[i].scheduleCheck == false) {
-                Submittals[i].scheduleCheck = true;
-            } else if (Submittals[i].masterscheduleRecord.Id == checkbox.get("v.text") && Submittals[i].scheduleCheck == true) {
-                Submittals[i].scheduleCheck = false;
-
-            }
-
-        }
-        component.set("v.masterSchedulesList", Submittals);
+        var selectedRecordId = event.target.value;
+        component.set("v.selectedRecordId", selectedRecordId);
+        console.log('selectedRecordId ===>',selectedRecordId);
     },
+    
+    // selectAll: function (component, event, helper) {
+    //     component.set("v.isError", false);
+    //     var selectedHeaderCheck = event.getSource().get("v.value");
+    //     var Submittals = JSON.parse(JSON.stringify(component.get("v.masterSchedulesList")));
+    //     var getAllId = component.find("checkContractor");
+    //     if (Submittals) {
+    //         if (Submittals.length) {
+    //             if (!Array.isArray(getAllId)) {
+    //                 if (selectedHeaderCheck == true) {
+    //                     component.find("checkContractor").set("v.value", true);
+    //                 } else {
+    //                     component.find("checkContractor").set("v.value", false);
+    //                 }
+    //             } else {
+    //                 if (selectedHeaderCheck == true) {
+    //                     for (var i = 0; i < getAllId.length; i++) {
+    //                         component.find("checkContractor")[i].set("v.value", true);
+    //                         var checkbox = component.find("checkContractor")[i].get("v.text");
+    //                         Submittals[i].ScheduleCheck = true;
 
-    selectAll: function (component, event, helper) {
-        component.set("v.isError", false);
-        var selectedHeaderCheck = event.getSource().get("v.value");
-        var Submittals = JSON.parse(JSON.stringify(component.get("v.masterSchedulesList")));
-        var getAllId = component.find("checkContractor");
-        if (Submittals) {
-            if (Submittals.length) {
-                if (!Array.isArray(getAllId)) {
-                    if (selectedHeaderCheck == true) {
-                        component.find("checkContractor").set("v.value", true);
-                    } else {
-                        component.find("checkContractor").set("v.value", false);
-                    }
-                } else {
-                    if (selectedHeaderCheck == true) {
-                        for (var i = 0; i < getAllId.length; i++) {
-                            component.find("checkContractor")[i].set("v.value", true);
-                            var checkbox = component.find("checkContractor")[i].get("v.text");
-                            Submittals[i].ScheduleCheck = true;
+    //                     }
+    //                 } else {
+    //                     for (var i = 0; i < getAllId.length; i++) {
+    //                         component.find("checkContractor")[i].set("v.value", false);
 
-                        }
-                    } else {
-                        for (var i = 0; i < getAllId.length; i++) {
-                            component.find("checkContractor")[i].set("v.value", false);
-
-                            var checkbox = component.find("checkContractor")[i].get("v.text");
-                            var Submittals = component.get("v.masterSchedulesList");
-                            Submittals[i].ScheduleCheck = false;
-                        }
-                    }
-                }
-            } else {
-                var i = 0;
-                if (Submittals.length) {
-                    if (selectedHeaderCheck == true) {
-                        component.find("checkContractor").set("v.value", true);
-                        var checkbox = component.find("checkContractor").get("v.text");
-                        Submittals[i].ScheduleCheck = true;
+    //                         var checkbox = component.find("checkContractor")[i].get("v.text");
+    //                         var Submittals = component.get("v.masterSchedulesList");
+    //                         Submittals[i].ScheduleCheck = false;
+    //                     }
+    //                 }
+    //             }
+    //         } else {
+    //             var i = 0;
+    //             if (Submittals.length) {
+    //                 if (selectedHeaderCheck == true) {
+    //                     component.find("checkContractor").set("v.value", true);
+    //                     var checkbox = component.find("checkContractor").get("v.text");
+    //                     Submittals[i].ScheduleCheck = true;
 
 
-                    } else {
-                        component.find("checkContractor").set("v.value", false);
+    //                 } else {
+    //                     component.find("checkContractor").set("v.value", false);
 
-                        var checkbox = component.find("checkContractor").get("v.text");
-                        var Submittals = component.get("v.masterSchedulesList");
-                        Submittals[i].ScheduleCheck = false;
+    //                     var checkbox = component.find("checkContractor").get("v.text");
+    //                     var Submittals = component.get("v.masterSchedulesList");
+    //                     Submittals[i].ScheduleCheck = false;
 
-                    }
-                }
-            }
-        }
+    //                 }
+    //             }
+    //         }
+    //     }
 
-    },
+    // },
 
     closeModel: function (component, event, helper) {
         component.set("v.isError", false);
@@ -153,180 +143,78 @@
 
         //window.open(baseURL + '.lightning.force.com/lightning/r/buildertek__Schedule__c/' + escape(recordId) + '/view', '_self');
     },
-
-    // importSchedule: function (component, event, helper) {
-    //     component.set("v.Spinner", true);
-    //     component.set("v.showMessage", true);
-    //     component.set("v.isError", false);
-    //     var url = location.href;
-    //     var baseURL = url.substring(0, url.indexOf('--', 0));
-    //     var SchedulesList = component.get("v.masterSchedulesList");
-    //     var ScheduleIds = [];
-    //     for (var i = 0; i < SchedulesList.length; i++) {
-    //         if (SchedulesList[i].scheduleCheck == true) {
-    //             ScheduleIds.push(SchedulesList[i].masterscheduleRecord.Id);
-    //         }
-    //     }
-    //     if (ScheduleIds.length > 0) {
-    //         var action = component.get("c.importMasterScheduleLines");
-    //         action.setParams({
-    //             scheduleRecIds: ScheduleIds,
-    //             recordId: component.get("v.RecordId")
-    //         });
-    //         action.setCallback(this, function (response) {
-    //             var state = response.getState();
-    //             if (state === "SUCCESS") {
-    //                 var result = response.getReturnValue();
-    //                 console.log('result ===>', { result });
-    //                 if (result.Status === 'Success') {
-    //                     component.set("v.isSucess", true);
-    //                     component.set("v.SucessMessage", result.Message);
-    //                     component.set("v.Spinner", false);
-    //                     component.set("v.showMessage", false);
-    //                     var baseURL = component.get("v.BaseURLs");
-    //                     var recordId = component.get("v.RecordId");
-    //                     if (component.get('v.isNewGantt')) {
-    //                         //window.open(baseURL + '.lightning.force.com/lightning/r/buildertek__Schedule__c/' + escape(recordId) + '/view', '_top');
-    //                         var workspaceAPI = component.find("workspace");
-    //                         if (workspaceAPI.getFocusedTabInfo()) {
-    //                             workspaceAPI.getFocusedTabInfo().then(function (response) {
-    //                                 var focusedTabId = response.tabId;
-    //                                 workspaceAPI.closeTab({ tabId: focusedTabId }).then(function (res) {
-    //                                     /*workspaceAPI.getFocusedTabInfo().then(function(response) {
-    //                                         var focusedTabId = response.tabId;
-    //                                        // $A.get('e.force:refreshView').fire();
-    //                                         window.open('/'+recordId,'_top')
-    //                                     }).catch(function(error) {
-    //                                         console.log(error);
-    //                                     });*/
-    //                                     if (res) {
-    //                                         window.open('/' + recordId, '_top')
-    //                                     }
-
-    //                                     //$A.enqueueAction(component.get('c.doInit'))
-    //                                     // $A.enqueueAction(navEvt.fire());
-    //                                 }).catch(function (error) {
-    //                                     console.log(error);
-    //                                 });
-
-    //                             }).catch(function (error) {
-    //                                 console.log(error);
-    //                                 var navEvt = $A.get("e.force:navigateToSObject");
-    //                                 navEvt.setParams({
-    //                                     "recordId": recordId,
-    //                                     "slideDevName": "detail"
-    //                                 });
-
-    //                                 $A.enqueueAction(component.get('c.doInit'))
-    //                                 $A.enqueueAction(navEvt.fire());
-    //                                 location.reload();
-    //                             });
-    //                         } else {
-    //                             window.open('/' + recordId, "_top");
-    //                             // window.open(baseURL + '.lightning.force.com/lightning/r/buildertek__Schedule__c/' + escape(recordId) + '/view', '_top');
-    //                         }
-
-    //                     } else {
-    //                         window.open('/apex/BT_Task_Manager?recordId=' + escape(recordId), '_self')
-    //                     }
-    //                     //window.open('/apex/BT_Task_Manager?recordId=' + escape(recordId), '_self')
-    //                 } else {
-    //                     component.set("v.Spinner", false);
-    //                     component.set("v.showMessage", false);
-    //                     component.set("v.isError", true);
-    //                     component.set("v.ErrorMessage", 'There are no schedule lines for the selected schedule.');
-    //                 }
-    //             }
-    //         });
-    //         $A.enqueueAction(action);
-    //     } else {
-    //         component.set("v.Spinner", false);
-    //         component.set("v.isError", true);
-    //         component.set("v.ErrorMessage", 'Please select at least one schedule to import.');
-
-    //     }
-    // },
     
     importSchedule: function (component, event, helper) {
-        component.set("v.Spinner", true);
-        component.set("v.showMessage", true);
-        component.set("v.isError", false);
-        var SchedulesList = component.get("v.masterSchedulesList");
-        console.log('SchedulesList ===>', typeof(SchedulesList));
-        debugger;
-        var ScheduleIds = [];
-        for (var i = 0; i < SchedulesList.length; i++) {
-            if (SchedulesList[i].scheduleCheck == true) {
-                ScheduleIds.push(SchedulesList[i].masterscheduleRecord.Id);
-            }
-        }
-        console.log('ScheduleIds ===>', { ScheduleIds });
-        if (ScheduleIds.length > 0) {
-            var action = component.get("c.createScheduleLineFromMasterSchedule");
-            action.setParams({
-                recordId: component.get("v.RecordId"),
-                masterId: ScheduleIds
-            });
-            action.setCallback(this, function (response) {
-                var state = response.getState();
-                if (state === "SUCCESS") {
-                    var result = response.getReturnValue();
-                    console.log('result ===>', { result });
-                    if (result.Status === 'Success') {
-                        component.set("v.isSucess", true);
-                        component.set("v.SucessMessage", result.Message);
-                        component.set("v.Spinner", false);
-                        component.set("v.showMessage", false);
-
-                        var recordId = component.get("v.RecordId");
-                        if (component.get('v.isNewGantt')) {
-                            var workspaceAPI = component.find("workspace");
-                            if (workspaceAPI.getFocusedTabInfo()) {
-                                workspaceAPI.getFocusedTabInfo().then(function (response) {
-                                    var focusedTabId = response.tabId;
-                                    workspaceAPI.closeTab({ tabId: focusedTabId }).then(function (res) {
-                                        if (res) {
-                                            window.open('/' + recordId, '_top')
-                                        }
-                                    }).catch(function (error) {
-                                        console.log(error);
-                                    });
-
-                                }).catch(function (error) {
-                                    console.log(error);
-                                    var navEvt = $A.get("e.force:navigateToSObject");
-                                    navEvt.setParams({
-                                        "recordId": recordId,
-                                        "slideDevName": "detail"
-                                    });
-
-                                    $A.enqueueAction(component.get('c.doInit'))
-                                    $A.enqueueAction(navEvt.fire());
-                                    location.reload();
-                                });
-                            } else {
-                                window.open('/' + recordId, "_top");
-                            }
-
-                        } else {
-                            window.open('/apex/BT_Task_Manager?recordId=' + escape(recordId), '_self')
-                        }
-                    } else {
-                        component.set("v.Spinner", false);
-                        component.set("v.showMessage", false);
-                        component.set("v.isError", true);
-                        component.set("v.ErrorMessage", 'There are no schedule lines for the selected schedule.');
-                    }
-                }
-            });
-            $A.enqueueAction(action);
-        } else {
+        var selectedRecordId = component.get("v.selectedRecordId");
+    
+        if (!selectedRecordId) {
             component.set("v.Spinner", false);
             component.set("v.isError", true);
             component.set("v.ErrorMessage", 'Please select at least one schedule to import.');
-
+            return;
         }
+    
+        component.set("v.Spinner", true);
+        component.set("v.showMessage", true);
+        component.set("v.isError", false);
+    
+        var action = component.get("c.createScheduleLineFromMasterSchedule");
+        action.setParams({
+            recordId: component.get("v.RecordId"),
+            masterId: selectedRecordId
+        });
+    
+        action.setCallback(this, function (response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                var result = response.getReturnValue();
+                console.log('result ===>', { result });
+                console.log('result.Status ===>', result.Status);
+                debugger;
+                if (result.Status === 'Success') {
+                    component.set("v.isSucess", true);
+                    component.set("v.SucessMessage", result.Message);
+                    component.set("v.Spinner", false);
+                    component.set("v.showMessage", false);
+    
+                    var recordId = component.get("v.RecordId");
+                    if (component.get('v.isNewGantt')) {
+                        var workspaceAPI = component.find("workspace");
+                        if (workspaceAPI && workspaceAPI.getFocusedTabInfo) {
+                            workspaceAPI.getFocusedTabInfo().then(function (response) {
+                                var focusedTabId = response.tabId;
+                                workspaceAPI.closeTab({ tabId: focusedTabId }).then(function () {
+                                    workspaceAPI.openTab({
+                                        url: '/lightning/r/YourObjectAPIName/' + recordId + '/view',
+                                        focus: true
+                                    }).then(function (tabId) {
+                                        workspaceAPI.refreshTab({
+                                            tabId: tabId,
+                                            includeAllSubtabs: true
+                                        });
+                                    });
+                                });
+                            }).catch(function (error) {
+                                console.log(error);
+                            });
+                        } else {
+                            window.open('/lightning/r/YourObjectAPIName/' + recordId + '/view', '_top');
+                        }
+                    } else {
+                        window.open('/'+recordId, "_top");
+                    }
+                } else {
+                    component.set("v.Spinner", false);
+                    component.set("v.showMessage", false);
+                    component.set("v.isError", true);
+                    component.set("v.ErrorMessage", 'There are no schedule lines for the selected schedule.');
+                }
+            }
+        });
+    
+        $A.enqueueAction(action);
     },
+    
     next: function (component, event, helper) {
         component.set("v.isError", false);
         var sObjectList = component.get("v.masterSchedulesList");
