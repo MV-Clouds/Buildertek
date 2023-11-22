a9713ea0abfb907fec323768f02d550e54eb724b
Jaimin Shah <jaiminshah555@gmail.com>
2023-11-22 14:45:21 +0530

    Lap3 : Getting vendors in Console

--- force-app/main/default/aura/Add_QuoteLine_from_Vendor/Add_QuoteLine_from_VendorController.js
+++ force-app/main/default/aura/Add_QuoteLine_from_Vendor/Add_QuoteLine_from_VendorController.js
@@ -1,7 +1,13 @@
 ({
     doInit : function(component, event, helper) {
-        console.log('Add_QuoteLine_from_VendorController.doInit');
         var QuoteId = component.get("v.quoteId");
         console.log('QuoteId: ' + QuoteId);
+        helper.getVendors(component, event, helper);
+    },
+
+    closeCmp : function(component, event, helper) {
+        component.set("v.openProductBoxwithVendor", false);
+     //    $A.get("e.force:closeQuickAction").fire() 
+ 
     },
 })
--- force-app/main/default/aura/Add_QuoteLine_from_Vendor/Add_QuoteLine_from_VendorHelper.js
+++ force-app/main/default/aura/Add_QuoteLine_from_Vendor/Add_QuoteLine_from_VendorHelper.js
@@ -1,5 +1,15 @@
 ({
-    helperMethod : function() {
-
-    }
+    getVendors : function(component, event, helper) {
+        console.log('getVendors');
+        var action = component.get("c.getVendors");
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                var vendors = response.getReturnValue();
+                console.log('vendors: ', vendors);
+                component.set("v.vendorList", vendors);
+            }
+        });
+        $A.enqueueAction(action);
+    },
 })
