171a17d13fb88b2657dde9debaca4213225a453c
Nishit <nishitsuthar123@gmail.com>
2023-11-29 17:10:08 +0530

    BT-1205 all the dev work is completed and assigned for testing

--- force-app/main/default/aura/syncVendors/syncVendorsController.js
+++ force-app/main/default/aura/syncVendors/syncVendorsController.js
@@ -1,10 +1,13 @@
 ({
     doInit : function(component, event, helper) {
-        console.log('doInit');
         helper.getVendorsAndScheduleItems(component);
     },
 
-    closeModel: function (component, event, helper) {
-        $A.get("e.force:closeQuickAction").fire();
+    processOperation : function(component, event, helper) {
+        helper.syncVendorsHelper(component);
+    },
+
+    closeModelCon: function (component, event, helper) {
+        helper.closeModel(component);
     }
 })
