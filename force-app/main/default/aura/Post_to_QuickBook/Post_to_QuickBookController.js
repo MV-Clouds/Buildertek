c865af8aa8d80e3dc6d184509e3b02861c90b744
nnirbhay <naviknirbhay@gmail.com>
2023-12-07 12:40:15 +0530

    BT-1224 <First Commit- this comit have logic for Customer/vendor and Po integration code>

---
+++ force-app/main/default/aura/Post_to_QuickBook/Post_to_QuickBookController.js
@@ -0,0 +1,58 @@
+({
+    doInit : function(component, event, helper) {
+        var id = component.get("v.recordId");
+        console.log(id);
+
+        var Objectname = component.get("v.sobjecttype");
+        console.log('Objectname---> ',Objectname);
+
+        if(Objectname == "Account"){
+            component.set("v.ShowAccountTypeOpt", true);
+        }
+
+        if (Objectname == 'buildertek__Purchase_Order__c') {
+            helper.createPO(component, event, helper);
+        }
+    },
+
+    handleRecordLoaded: function(component, event, helper) {
+
+        console.log(component.get("v.BTAccountType.buildertek__BT_Account_Type__c"));
+        var BTAccountType = component.get("v.BTAccountType")["buildertek__BT_Account_Type__c"];
+        var QBID = component.get("v.BTAccountType")["buildertek__QB_Type__c"];
+        var QBType = component.get("v.BTAccountType")["buildertek__QB_Type__c"];
+        if(BTAccountType == "Customer" || BTAccountType == "Vendor"){
+            component.set("v.AccountType", BTAccountType)
+        }
+        if(QBID == null){
+            helper.PostAccountToQuickbook(component, event, helper);
+        }
+        else if(QBID != null){
+            if(BTAccountType == QBType){
+                helper.PostAccountToQuickbook(component, event, helper);
+            }
+            else{
+                component.find('notifLib').showNotice({
+                    "variant": "error",
+                    "header": "Error",
+                    "message":  'This Account is posted as ' + QBType + ' in Quickbook, So You can not Post this Account as ' + BTAccountType +  ' again',
+                }); 
+                $A.get("e.force:closeQuickAction").fire();
+            }
+        }
+
+    },
+
+    handleChangeAccountType: function(component, event, helper){
+        var auraIdField = event.getSource().getLocalId();
+
+        console.log('Selected account type : ',component.find(auraIdField).get("v.value"));
+
+        component.set("v.AccountType",component.find(auraIdField).get("v.value"));
+    },
+
+    CloseQuickAction: function(component, event, helper){
+        $A.get("e.force:closeQuickAction").fire();
+    },
+
+})
---
+++ force-app/main/default/aura/Post_to_QuickBook/Post_to_QuickBookHelper.js
@@ -0,0 +1,138 @@
+({
+    createPO : function(component, event, helper) {
+        var action = component.get("c.Create_Vendor_POItem_PO_in_QB_AuraCallout");
+        action.setParams({
+            recordId : component.get("v.recordId"),
+            SyncObjName : component.get("v.sobjecttype")
+        });
+        
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            console.log('state ==> ' + state);
+            
+            if(state === "SUCCESS") {
+                var result = response.getReturnValue();
+                console.log('return value ==> '+ result);
+                $A.get("e.force:closeQuickAction").fire();
+                if(result == 'success'){
+            		component.find('notifLib').showNotice({
+    		            "variant": "success",
+    		            "header": "Success",
+    		            "message": "Completed",
+    		        });    
+                }else if(result == 'no_polines') {
+                    component.find('notifLib').showNotice({
+    		            "variant": "error",
+    		            "header": "Error",
+    		            "message": 'There are no PO Line(s) associated with the PO.',
+    		        });    
+                }else if(result == 'no_vendor_account'){
+                    component.find('notifLib').showNotice({
+    		            "variant": "error",
+    		            "header": "Error",
+    		            "message": 'There are no Vendor account associated with the PO.',
+    		        });  
+                }else{
+                    component.find('notifLib').showNotice({
+    		            "variant": "error",
+    		            "header": "Error",
+    		            "message": 'Something Went Wrong !!!',
+    		        });  
+                }
+                
+            }
+        });
+        
+        $A.enqueueAction(action);	
+    },
+
+    PostAccountToQuickbook: function(component, event, helper){
+        var accountType = component.get("v.AccountType");
+        if(accountType == 'Customer'){
+            component.set("v.ShowAccountTypeOpt", false);
+            helper.Post_Customer_ToQBHelper(component, event, helper);
+        }
+        else if(accountType == 'Vendor'){
+            component.set("v.ShowAccountTypeOpt", false);
+            helper.Post_Vendor_ToQBHelper(component, event, helper);
+        }
+        else{
+            component.find('notifLib').showNotice({
+                "variant": "error",
+                "header": "Error",
+                "message": 'Please Select at least on Account Type!',
+            });    
+        }
+    },
+
+    Post_Customer_ToQBHelper: function(component, event, helper){
+        console.log("Inside Customer Integration helper");
+        var action = component.get("c.Create_Customer_In_QB_AuraCallout");
+        action.setParams({
+            AccoountId : component.get("v.recordId"),
+        });
+        
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            console.log('Customer state ==> ' + state);
+            var result = response.getReturnValue();
+            console.log('Customer result ==> ' + result);
+   
+            if(result == 'Completed'){
+                component.find('notifLib').showNotice({
+                    "variant": "success",
+                    "header": "Success",
+                    "message": 'Completed',
+                }); 
+                $A.get("e.force:closeQuickAction").fire();
+            }
+            else{
+                component.find('notifLib').showNotice({
+                    "variant": "error",
+                    "header": "Error",
+                    "message": 'Something Went Wrong!',
+                }); 
+                $A.get("e.force:closeQuickAction").fire();
+            }
+        });
+        $A.enqueueAction(action);
+    },
+
+    Post_Vendor_ToQBHelper: function(component, event, helper){
+        console.log("Inside Vendor Integration helper");
+        var action = component.get("c.Create_Vendor_In_QB_AuraCallout");
+        action.setParams({
+            AccoountId : component.get("v.recordId"),
+            SyncObjName : component.get("v.sObjectName")
+        });
+        
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            console.log('Vendor state ==> ' + state);
+            var result = response.getReturnValue();
+            console.log('Vendor result ==> ' + result);
+
+            if(result == 'Completed'){
+                component.find('notifLib').showNotice({
+                    "variant": "success",
+                    "header": "Success",
+                    "message": 'Completed',
+                }); 
+                $A.get("e.force:closeQuickAction").fire();
+            }
+            else{
+                component.find('notifLib').showNotice({
+                    "variant": "error",
+                    "header": "Error",
+                    "message": 'Something Went Wrong!',
+                }); 
+                $A.get("e.force:closeQuickAction").fire();
+            }
+        });
+
+        $A.enqueueAction(action);
+    },
+
+
+
+})
