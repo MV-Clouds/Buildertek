97b32b64db35e8c3b23509df38ad25e60bea76f3
nnirbhay <naviknirbhay@gmail.com>
2024-02-08 18:54:24 +0530

    BT-1393 BUIL-3883 developed logic to solve TOO many SOQL query on for clone project

--- force-app/main/default/aura/btCloneProject_recCreatePage/btCloneProject_recCreatePageController.js
+++ force-app/main/default/aura/btCloneProject_recCreatePage/btCloneProject_recCreatePageController.js
@@ -19,6 +19,12 @@
 
 		  helper.getFieldSetHelper(component, event, helper);
       helper.setTabIconHelper(component, event, helper);
+      if(component.get("v.objsToCreateList").length > 0){
+        helper.helperCreateRollBackProject(component, event, helper);
+      }
+      else{
+        component.set("v.cloneFlag", true);
+      }
     },
 
     loadSuccess: function(component, event, helper){
@@ -27,35 +33,59 @@
     },
 
     handleError: function(component, event, helper){
-      console.log('error in recordedit from');
-      helper.toastHelper(component, event, helper, 'Error', 'error while loading page', 'error', 3000);
-      component.set("v.isLoading", false);
+      try {
+        component.set("v.isErrorModal", true);
+        console.log('error in recordedit from output: ', JSON.parse(JSON.stringify(event.getParam("output"))));
+        console.log('error in recordedit from message : ', JSON.parse(JSON.stringify(event.getParam("message"))));
+        var errorOutput = JSON.parse(JSON.stringify(event.getParam("output")));
+        var errorMessages = [];
+        var message;
+        // When get error message during record creation...
+        if(errorOutput.fieldErrors && errorOutput.errors){
+          // When got error message from apex exception other than fields error
+          if(errorOutput.errors.length > 0){
+            var errorList = errorOutput.errors;
+            for(var i in errorList){
+              errorMessages.push(errorList[i].message);
+            }
+          }
+          // If got error message form field filter validation
+          else if(Object.keys(errorOutput.fieldErrors).length > 0){
+            var fieldError = errorOutput.fieldErrors;
+            if(fieldError.Id.length > 0){
+              var errorDetail = fieldError.Id;
+              for(var i in errorDetail){
+                errorMessages.push(errorDetail[i].message);
+              }
+            }
+          }
+          else{
+            errorMessages.push(event.getParam("message"));
+          }
+          component.set("v.isError", true);
+          component.set("v.errorMessages", errorMessages);
+        }
+        else{
+          helper.toastHelper(component, event, helper, 'Error', 'Error while loading record create page.', 'error', 3000);
+        }
+        component.set("v.isLoading", false);
+      } catch (error) {
+        console.log('error in handleError : ', error.stack);
+        
+      }
     },
                         
     createCloneProject : function(component, event, helper){
       try {
         event.preventDefault();
-        var fields = event.getParam("fields");
-        fields['buildertek__Source_Project__c'] = component.get("v.Source_Project");
-        console.log('fields', JSON.parse(JSON.stringify(fields)));
-        // console.log('recordId : ' + component.get("v.recordId"));
-        // var updatedData = JSON.stringify(fields);
-        if(fields['Name'] == null || fields['Name'] ==''){
-          helper.toastHelper(component, event, helper, 'Error', 'Please insert project name', 'error', 3000);
-        }
-        else if(fields['buildertek__Customer__c'] == null || fields['buildertek__Customer__c'] ==''){
-          helper.toastHelper(component, event, helper, 'Error', 'Please Assign Customer!', 'error', 3000);
-        }
-        else{
-          component.set("v.isLoading", true);
-          component.find('recordEditForm').submit(fields);
-        }
+        helper.helperCreateCloneProject(component, event, helper);
         
       } catch (error) {
         console.log("error in createCloneProject : ", error.stack);
       }
     },
     
+    // Once Project created this method will automatically... because we used standard submit method....
     handleSuccess: function(component, event, helper){
       try {
         console.log("inside success create clone project");
@@ -65,6 +95,7 @@
         var record = event.getParams();  
         console.log('record Id ',record.id);
         var payload = event.getParams().response;
+        console.log('response : ', payload);
         var cloneProjectId = (payload.id).replace('"','').replace('"',''); 
         console.log('clone project Id : ', cloneProjectId);
 
@@ -86,7 +117,9 @@
         else{
           helper.toastHelper(component, event, helper, 'Error', 'error while cloning project', 'error', 3000);
           component.set("v.isLoading", false);
+          helper.closeModelHelper(component, event, helper, cloneProjectId);
         }
+
         
       } catch (error) {
         console.log("error in handleSuccess : ", error.stack);
@@ -97,6 +130,15 @@
     closeModel : function(component, event, helper){
       helper.closeModelHelper(component, event, helper, component.get("v.recordId"));
     },
+
+    handleErrorModal: function(component, event, helper){
+      try {
+        component.set("v.isErrorModal", !component.get("v.isErrorModal"));
+      } catch (error) {
+        console.log('error in handleErrorModal : ', error.stack);
+        
+      }
+    }
 
     
 })
--- force-app/main/default/aura/btCloneProject_recCreatePage/btCloneProject_recCreatePageHelper.js
+++ force-app/main/default/aura/btCloneProject_recCreatePage/btCloneProject_recCreatePageHelper.js
@@ -14,13 +14,13 @@
                 if(result != null){
                   var listOfFields =  JSON.parse(result);
                   var sourceProject = component.get("v.projects").length > 0 ? component.get("v.projects")[0] : {};
-                  console.log('sourceProject : ', sourceProject);
+                  // console.log('sourceProject : ', sourceProject);
   
                   for(var i in listOfFields){
                     var fieldName = listOfFields[i].name;
                     listOfFields[i]['value'] = sourceProject[fieldName];
                   }
-                  console.log('listOfFields updated : ', listOfFields);
+                  // console.log('listOfFields updated : ', listOfFields);
                   component.set("v.listOfFields", listOfFields);
                 }
                 else{
@@ -36,6 +36,47 @@
         }
     },
 
+    helperCreateCloneProject: function(component, event, helper){
+      console.log('cloneFlag : ', component.get("v.cloneFlag"));
+
+      //  check --> process of checking exception for child(rollback method) is completed
+      //    if yes....
+      //        check --> did we get any exception from child object cloning)
+      //              if yes... show error message
+      //              else... create project along with all child object
+      //    else...
+      //        turn on spinner... And recall this method again after 2...
+      //        again check this process...
+       
+        if(component.get("v.cloneFlag") == true){
+          if(component.get("v.errorMessages").length > 0){
+            component.set("v.isLoading", true);
+            window.setTimeout(function () {
+              component.set("v.isError", true);
+              component.set("v.isErrorModal", true);
+              component.set("v.isLoading", false);
+            }, 500)
+          }
+          else{
+            var fields = event.getParam("fields");
+            fields['buildertek__Source_Project__c'] = component.get("v.Source_Project");
+            console.log('fields', JSON.parse(JSON.stringify(fields)));
+  
+            component.set("v.isLoading", true);
+            component.set("v.isError", false);
+            component.set("v.isErrorModal", false);
+            component.find('recordEditForm').submit(fields);
+          }
+        }       
+         else{
+          component.set("v.isLoading", true);
+          window.setTimeout(function () {
+            helper.helperCreateCloneProject(component, event, helper);
+          }, 2000)
+          
+        }
+    },
+
     helperCloneChildObj: function(component, event, helper, cloneProjectId, childObj, isLast){
 
       console.log('inside helper clone project');
@@ -44,6 +85,7 @@
           clonedProjectId : cloneProjectId,
           sourceProjectId : component.get("v.recordId"),
           objName : childObj,
+          roll_back : false,
         });
         action.setCallback(this, function(response){
             var result = response.getReturnValue();
@@ -127,5 +169,79 @@
                   type: Type
               }, Duration);
           toast.fire();
+      },
+
+      // This method will create Project(for temparary basis) for checking child object is throwing any exception or not during the clone project....
+      helperCreateRollBackProject: function(component, event, helper){
+        try {
+          var action = component.get("c.createRollBackProject");
+          action.setParams({
+            sourceProjectId : component.get("v.recordId"),
+            ObjectList : component.get("v.objsToCreateList"),
+            roll_back : false
+          });
+          action.setCallback(this, function(response){
+            var result = response.getReturnValue();
+            console.log('reult of createRollBackProject : ', result);
+            if(result != null){
+              var objsToCreateList = component.get('v.objsToCreateList');
+              for(var i in objsToCreateList){
+                var isLast = i == (objsToCreateList.length - 1) ? true : false;
+                helper.checkCloneChildRollBack(component, event, helper, result, objsToCreateList[i], isLast);
+              }
+            }
+            else{
+              component.set("v.cloneFlag", true);
+              console.log('error in create roll back project');
+            }
+          });
+          $A.enqueueAction(action);
+          
+        } catch (error) {
+          console.log('error in createRollBackProject : ', error.stack);
+          
+        }
+      },
+
+      // This method use to check the is cloning of any child object thow any exception...
+      // if yes... exception message will edded in list and will dispay on UI on save button click
+      // else.. it will set "cloneFlag" to "True" for further process...
+      checkCloneChildRollBack: function(component, event, helper, rollBackProjectId, childObj, isLast){
+        var action = component.get("c.cloneChildObj")
+        action.setParams({
+          clonedProjectId : rollBackProjectId,
+          sourceProjectId : component.get("v.recordId"),
+          objName : childObj,
+          roll_back : true,
+        });
+        action.setCallback(this, function(response){
+            var result = response.getReturnValue();
+            console.log(childObj ," --roll back result-- ", result);
+            var errorMessages = component.get("v.errorMessages");
+            if(result != 'success'){
+              errorMessages.push(childObj+': '+ result);
+            }
+            if(errorMessages.length > 0){
+              component.set("v.errorMessages", errorMessages);
+            }
+            if(isLast){
+              helper.deleteRollbackProject(component, event, helper, rollBackProjectId);
+              component.set("v.cloneFlag", true);
+            }
+        });
+		    $A.enqueueAction(action);
+      },
+
+      // This method used delete craete project(Roll Back Project) for purposed of child object exception checking...
+      deleteRollbackProject: function(component, event, helper, rollBackProjectId){
+        var action = component.get("c.deleteRollbackProject")
+        action.setParams({
+          rollBackProjectId : rollBackProjectId,
+        });
+        action.setCallback(this, function(response){
+            var result = response.getReturnValue();
+            console.log('result of deleteRollbackProject : ', result);
+        });
+		    $A.enqueueAction(action);
       }
 })
