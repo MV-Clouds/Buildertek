6b56020fbad099442704ecbe8ac264e13d423757
Nirbhay Navik <nirbhay_navik@Nirbhays-MacBook-Air.local>
2024-02-23 15:12:58 +0530

    BT-1445-BUIL-3937 Front end done

--- force-app/main/default/aura/BT_createVoidPO/BT_createVoidPOController.js
+++ force-app/main/default/aura/BT_createVoidPO/BT_createVoidPOController.js
@@ -1,5 +1,13 @@
 ({
-    myAction : function(component, event, helper) {
+    doInit : function(component, event, helper) {
 
-    }
+    },
+
+    handleCreateVoidPO: function(component, event, helper) {
+        component.set("v.createVoidPoFlag", true);
+    },
+
+    handleCancel: function(component, event, helper){
+        $A.get("e.force:closeQuickAction").fire() 
+    },
 })
