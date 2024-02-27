11dc83e7c202e53d5da43871e63976cc3082da44
Nirbhay Navik <nirbhay_navik@Nirbhays-MacBook-Air.local>
2024-02-27 12:51:16 +0530

    BT-1445-BUIL-3937 karan singh changes on 26th feb...

--- force-app/main/default/aura/BT_createVoidPO/BT_createVoidPOController.js
+++ force-app/main/default/aura/BT_createVoidPO/BT_createVoidPOController.js
@@ -1,15 +1,5 @@
 ({
     doInit : function(component, event, helper) {
-        console.log('Object API Name : ', component.get("v.sObjectName"));
-
-        var workspaceAPI = component.find("workspace");
-        workspaceAPI.getFocusedTabInfo().then((response) => {
-            component.set("v.parentTabId", response.parentTabId);
-        })
-    },
-
-    handleCreateVoidPO: function(component, event, helper) {
-        component.set("v.createVoidPoFlag", true);
 
         var action = component.get("c.createVoidPO");
         action.setParams({
@@ -18,12 +8,11 @@
         action.setCallback(this, function(response){
             var result = response.getReturnValue();
             if(result.status == 'success'){
-                var workspaceAPI = component.find("workspace");
-                workspaceAPI.openSubtab({
-                    parentTabId: component.get("v.parentTabId"),
-                    url: `/lightning/r/${component.get("v.sObjectName")}/${result.createRecordId}/view`,
-                    focus: true
-                });
+                var sObjectEvent = $A.get("e.force:navigateToSObject");
+                sObjectEvent.setParams({
+                    "recordId": result.createRecordId
+                })
+                sObjectEvent.fire();
                 helper.showToastHelper(component, event, helper,result.status , result.message , result.status , 3000 );
             }
             else if(result.status == 'error'){
@@ -34,7 +23,4 @@
         $A.enqueueAction(action);
     },
 
-    handleCancel: function(component, event, helper){
-        $A.get("e.force:closeQuickAction").fire() 
-    },
 })
