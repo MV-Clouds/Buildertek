b8e4508421ae370e6a9ecf363daca38d2f75dbce
Nirbhay Navik <nirbhay_navik@Nirbhays-MacBook-Air.local>
2024-02-27 19:28:49 +0530

    development phase 2...

--- force-app/main/default/lwc/scheduleResourcesAssignee/scheduleResourcesAssignee.js
+++ force-app/main/default/lwc/scheduleResourcesAssignee/scheduleResourcesAssignee.js
@@ -1,87 +1,63 @@
-import { LightningElement, api, track } from 'lwc';
+import { LightningElement, api, track, wire} from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import { loadStyle } from 'lightning/platformResourceLoader';
 import myResource from '@salesforce/resourceUrl/ScheduleLWCCss';
-import fetchResouceData_byVendor from '@salesforce/apex/scheduleAssignResources.fetchResouceData_byVendor';
 import fetchResouceData from '@salesforce/apex/scheduleAssignResources.fetchResouceData';
+import { NavigationMixin } from 'lightning/navigation';
+import { IsConsoleNavigation, getFocusedTabInfo, closeTab } from 'lightning/platformWorkspaceApi';
 
-export default class ScheduleResourcesAssignee extends LightningElement {
+export default class ScheduleResourcesAssignee extends NavigationMixin(LightningElement) {
 
     @api vendorId;
     @api scheduleId;
     @api vendorResourceId;
     
     @track isLoading = false;
+    @track IsTaskAvailable = false;
+    @track customeMessge = 'Loading...'
     @track scheduleDataWrapper = [];
     @track groupedScheduleData = {};
-    @track contractors = {};
+    @track vendor = {};
 
     @track resouceName = '';
     @track resouceTaks = [];
-    @track resourceList;
+    @track resourceList = [];
+
 
     connectedCallback() {
         loadStyle(this, myResource);
+
+        this.vendorId = this.vendorId ? this.vendorId : null;
+        this.vendorResourceId = this.vendorResourceId ? this.vendorResourceId : null;
 
         console.log('this.c__vendorId : ', this.vendorId);
         console.log('this.c__scheduleId : ', this.scheduleId);
         console.log('this.c__vendorResourceId : ', this.vendorResourceId);
 
-        if(this.vendorId){
-            this.getResourceData_byVendor();
-        }
-        else if(this.vendorResourceId){
-            this.getResourceData();
-        }
+        this.getResourceData();
     }
 
-    getResourceData(){
-        try {
-            this.isLoading=true;
-
-            fetchResouceData({vendorResourceId : this.vendorResourceId})
-            .then(result => {
-                if (result.status == 'success') {
-                    var resourceDataRaw = result.resourceData;
-                    resourceDataRaw.isTaskAssigned = resourceDataRaw.taskList > 0 ? true : false;
-                    resourceDataRaw.taskList.forEach(task => {
-                        task.DependancyName = task.hasOwnProperty('buildertek__Dependency__r') ? task.buildertek__Dependency__r.Name : null;
-                    })
-                    this.resourceList.push(result.resourceData);
-                    console.log('resourceList : ', JSON.parse(JSON.stringify(this.resourceList)));
-                    this.isLoading = false;
-                }
-                else if(result.status == 'error'){
-                    this.showToastUtility('Error',result.returnMessge, result.status);
-                    this.isLoading = false;
-                }
-            })
-            .catch(error =>{
-                this.isLoading = false;
-                console.log('error to call apex fetchResouceData', {error});
-            })
-            
-        } catch (error) {
-            console.log('error in getResourceData : ', error.stack);
-            
-        }
-    }
-
-    getResourceData_byVendor() {
+    getResourceData() {
         try {
             this.isLoading = true;
-            fetchResouceData_byVendor({vendorId: this.vendorId })
+            fetchResouceData({vendorId: this.vendorId,  vendorResourceId : this.vendorResourceId})
                 .then(result => {
                     if (result.status == 'success') {
                         console.log('resourceDataList :', JSON.parse(JSON.stringify(result.resourceDataList)));
                         var resourceListRaw = JSON.parse(JSON.stringify(result.resourceDataList));
-                        resourceListRaw.forEach(ele => {
-                            ele.isTaskAssigned = ele.taskList.length > 0 ? true : false;
-                            ele.taskList.forEach(task => {
-                                task.DependancyName = task.hasOwnProperty('buildertek__Dependency__r') ? task.buildertek__Dependency__r.Name : '--';
+                        this.IsTaskAvailable = resourceListRaw.length > 0 ? true : false;
+                        this.customeMessge = resourceListRaw.length > 0 ? '' : 'No task available';
+                        if(resourceListRaw.length > 0){
+                            resourceListRaw.forEach(ele => {
+                                ele.isTaskAssigned = ele.taskList.length > 0 ? true : false;
+                                ele.taskList.forEach(task => {
+                                    task.DependancyName = task.hasOwnProperty('buildertek__Dependency__r') ? task.buildertek__Dependency__r.Name : '--';
+                                })
                             })
-                        })
-                        this.resourceList = resourceListRaw;
+                            this.resourceList = resourceListRaw;
+                            this.vendor = result.vendor ? result.vendor : {Id: '', Name : '---'};
+
+                        }
                         console.log('resourceList : ', JSON.parse(JSON.stringify(this.resourceList)));
                     }
                     else if(result.status == 'error'){
@@ -103,16 +79,41 @@ export default class ScheduleResourcesAssignee extends LightningElement {
 
     handleBack(){
         try {
-            alert('Work in Progress, be patience, Thank you :) ');
+            getFocusedTabInfo().then((tabInfo) => {
+                closeTab(tabInfo.tabId);
+            }).catch(function(error) {
+                console.log(error);
+            });
+
+            this[NavigationMixin.Navigate]({
+                type: 'standard__recordPage',
+                attributes: {
+                    recordId : this.scheduleId,
+                    objectApiName: 'buildertek__Schedule__c',
+                    actionName: 'view'
+                },
+            });
         } catch (error) {
             console.log('error in handleBack : ', error.stack);
             
         }
     }
 
-
-
-
+    redirectTOsobject(event){
+        try {
+            this[NavigationMixin.Navigate]({
+                type: 'standard__recordPage',
+                attributes: {
+                    recordId : event.currentTarget.dataset.id,
+                    objectApiName: event.currentTarget.dataset.objname,
+                    actionName: 'view'
+                },
+            });
+        } catch (error) {
+            console.log('error in redirectTOsobject : ', error.stack);
+            
+        }
+    }
 
     showToastUtility(Title, Message, Type){
         try {
