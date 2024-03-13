da3be1ce0737f8efcf5cfa24d8877431713d1761
Jaimin Shah <jaiminshah555@gmail.com>
2024-03-13 11:52:51 +0530

    1303start

--- force-app/main/default/aura/BT_Mass_Update_TimeSheet/BT_Mass_Update_TimeSheetController.js
+++ force-app/main/default/aura/BT_Mass_Update_TimeSheet/BT_Mass_Update_TimeSheetController.js
@@ -9,6 +9,26 @@
     onMassUpdateSave: function(component, event, helper){
         component.set("v.Spinner", true);
         console.log('onMassUpdateSave');
+        var timeSheetEntries = component.get('v.timeSheetEntries');
+        var valid = timeSheetEntries.every(function(entry){
+            console.log('entry.buildertek__Duration_Manual__c', entry.buildertek__Duration_Manual__c);
+            return (entry.buildertek__Duration_Manual__c === null || !isNaN(entry.buildertek__Duration_Manual__c) || entry.buildertek__Duration_Manual__c === '' || entry.buildertek__Duration_Manual__c === undefined);
+        });
+        if(!valid){
+            component.set("v.Spinner", false);
+            var toastEvent = $A.get("e.force:showToast");
+            if(toastEvent){
+                toastEvent.setParams({
+                    "title": "Error",
+                    "message": "Duration should be a number.",
+                    "type": "error",
+                    "duration": 1000
+                });
+                toastEvent.fire();
+            }
+            return;
+        }
+        debugger;
         helper.updateTimeSheetEntries(component, event, helper);
     },
 
