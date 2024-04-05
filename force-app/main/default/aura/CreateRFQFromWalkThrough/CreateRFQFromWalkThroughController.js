a6a8e986ecee83d552489a7ea95002dffbb4a326
Nishit <nishitsuthar123@gmail.com>
2024-04-05 15:46:54 +0530

    IBT-1498 added comboBox

--- force-app/main/default/aura/CreateRFQFromWalkThrough/CreateRFQFromWalkThroughController.js
+++ force-app/main/default/aura/CreateRFQFromWalkThrough/CreateRFQFromWalkThroughController.js
@@ -1,5 +1,16 @@
 ({
     doInit : function(component, event, helper) {
-        helper.createRFQ(component, helper);
+        // helper.createRFQ(component, helper);
+        var options = [
+            { value: 'All', label: 'All', description: 'Create all RFQ lines' },
+            { value: 'tradeType', label: 'Trade Type', description: 'Create RFQ lines with Trade Type only' },
+            { value: 'costCode', label: 'Cost Code', description: 'Create RFQ lines with Cost Code only' },
+            { value: 'section', label: 'Section', description: 'Create RFQ lines with Section only' }
+        ];
+        component.set("v.statusOptions", options);
 	},
+
+    handleOptionSelected: function (cmp, event) {
+        cmp.set("v.optionSelected", true);
+    }
 })
