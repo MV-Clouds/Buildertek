a657a64bc3a913232f0a0d99b2c750c6eaeb24e4
Nishit <nishitsuthar123@gmail.com>
2024-04-08 18:45:22 +0530

    IBT-1508 development work completed along with UNIT testing

--- force-app/main/default/aura/CreateRFQFromWalkThrough/CreateRFQFromWalkThroughHelper.js
+++ force-app/main/default/aura/CreateRFQFromWalkThrough/CreateRFQFromWalkThroughHelper.js
@@ -1,37 +1,70 @@
 ({
-	createRFQ : function(component, helper){
+    createRFQ: function (component, helper) {
+        console.log('createRFQ');
         var action = component.get("c.createRFQFromWT");
-        action.setParams({ walkThroughId : component.get("v.recordId") });
-        
-        action.setCallback(this, function(response) {
+        action.setParams({ walkThroughId: component.get("v.recordId") });
+
+        action.setCallback(this, function (response) {
             var state = response.getState();
-            
-            if(state === "SUCCESS") {
+
+            if (state === "SUCCESS") {
                 var result = response.getReturnValue();
                 $A.get("e.force:closeQuickAction").fire();
-                if(result.Status === 'Success'){
+                if (result.Status === 'Success') {
                     component.find('notifLib').showNotice({
                         "variant": "success",
                         "header": "Success",
                         "message": result.Message,
-                        closeCallback: function() {
+                        closeCallback: function () {
                             $A.get('e.force:refreshView').fire();
-                            var event = $A.get('e.force:navigateToSObject' );
+                            var event = $A.get('e.force:navigateToSObject');
                             event.setParams({
-                                'recordId' : result.newRecordId
+                                'recordId': result.newRecordId
                             }).fire();
                         }
-                    }); 
-                }else{
+                    });
+                } else {
                     component.find('notifLib').showNotice({
                         "variant": "error",
                         "header": "Error",
                         "message": result.Message
-                    });  
+                    });
                 }
             }
         });
-        
-        $A.enqueueAction(action);   
-	}
+
+        $A.enqueueAction(action);
+    },
+
+    groupTradeTypeHelper: function (component, selectedValue, helper) {
+        console.log('record Id ', component.get("v.recordId"));
+        console.log('selectedValue ', selectedValue);
+        var action = component.get("c.groupRrqTradeType");
+        action.setParams({ walkThroughId: component.get("v.recordId"), grpType: selectedValue });
+
+        action.setCallback(this, function (response) {
+            var state = response.getState();
+            console.log('state: ', state);
+            if (state === "SUCCESS") {
+                var result = response.getReturnValue();
+                console.log('result: ', result);
+                $A.get("e.force:closeQuickAction").fire();
+                if (result.Status === 'Success') {
+                    component.find('notifLib').showNotice({
+                        "variant": "success",
+                        "header": "Success",
+                        "message": result.Message,
+                    });
+                } else {
+                    component.find('notifLib').showNotice({
+                        "variant": "error",
+                        "header": "Error",
+                        "message": result.Message
+                    });
+                }
+            }
+        });
+
+        $A.enqueueAction(action);
+    },
 })
