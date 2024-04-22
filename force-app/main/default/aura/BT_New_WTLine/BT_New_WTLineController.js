91772fcc17bb30311049e7807e7736c0fe04d9bf
Jaimin Shah <jaiminshah555@gmail.com>
2024-04-22 18:17:13 +0530

    done

--- force-app/main/default/aura/BT_New_WTLine/BT_New_WTLineController.js
+++ force-app/main/default/aura/BT_New_WTLine/BT_New_WTLineController.js
@@ -40,24 +40,53 @@
     },
 
     handleSubmit : function (component, event, helper) {
-        component.set("v.isDisabled", true);
-        component.set("v.isLoading", true);
-        event.preventDefault(); // Prevent default submit
+        component.set("v.Spinner", true);
+        event.preventDefault(); 
         var fields = event.getParam("fields");
-        console.log('fields: ' + fields);
-        var allData = JSON.stringify(fields);
-        console.log('allData: ' + allData);
+        var allData = JSON.parse(JSON.stringify(fields)); 
+        var recordType = component.get("v.selectedRecordTypeName");
+        if(recordType === 'Product'){
+            if(allData.buildertek__Product__c === undefined || allData.buildertek__Product__c === '' || allData.buildertek__Product__c === null){
+                component.set("v.Spinner", false);
+                var toastEvent = $A.get("e.force:showToast");
+                toastEvent.setParams({
+                    title: "Error",
+                    message: "Please select a Product",
+                    type: "error"
+                });
+                toastEvent.fire();
+                return;
+            }
+        }
+        if(recordType === 'No Product'){
+            if(allData.buildertek__Description__c === undefined || allData.buildertek__Description__c === '' || allData.buildertek__Description__c === null){
+                component.set("v.Spinner", false);
+                var toastEvent = $A.get("e.force:showToast");
+                toastEvent.setParams({
+                    title: "Error",
+                    message: "Please enter a Description",
+                    type: "error"
+                });
+                toastEvent.fire();
+                return;
+            }
+        }
+        console.log('allData: ' , allData);
         var action = component.get("c.saveData");
         action.setParams({
             allData : allData
         });
-        action.setCallback(this, function(response) {
-            var state = response.getState();
-            if (state === "SUCCESS") {
-                console.log('Saved successfully');
+        action.setCallback(this, function(response){
+            console.log(response.getState());
+            if(response.getState() == 'SUCCESS') {            
+                var result = response.getReturnValue();
+                console.log({result});
+                component.set("v.Spinner", false);
                 $A.get("e.force:closeQuickAction").fire();
+                $A.get('e.force:refreshView').fire();
             }
         });
-        // $A.enqueueAction(action);
+        $A.enqueueAction(action);
+
     }
 })
