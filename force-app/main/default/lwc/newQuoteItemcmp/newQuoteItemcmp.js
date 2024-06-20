f2243d41e8f988a8b2e40abd8050fd8580a95000
Jaimin Shah <jaiminshah555@gmail.com>
2024-06-20 16:26:54 +0530

    Table done

--- force-app/main/default/lwc/newQuoteItemcmp/newQuoteItemcmp.js
+++ force-app/main/default/lwc/newQuoteItemcmp/newQuoteItemcmp.js
@@ -1,59 +1,31 @@
-import { LightningElement, track } from 'lwc';
+import { LightningElement, track, api } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import getallData from '@salesforce/apex/QuotePage.getallData';
 
 export default class NewQuoteItemcmp extends LightningElement {
     isInitalRender = true;
-    @track data = [
-        {
-            groupName: 'Administration',
-            items: [
-                {
-                    id: 1,
-                    productName: 'ABC',
-                    costCode: 'B2010.21',
-                    quantity: 1,
-                    unitCost: 100.00,
-                    markup: 75,
-                    unitSalesPrice: 175.00,
-                    totalPreTax: 175.00,
-                    tax: 0.00
-                }
-            ]
-        },
-        {
-            groupName: 'No Grouping',
-            items: [
-                {
-                    id: 2,
-                    productName: 'BCD',
-                    costCode: 'B2010.21',
-                    quantity: 1,
-                    unitCost: 100.00,
-                    markup: 75,
-                    unitSalesPrice: 175.00,
-                    totalPreTax: 175.00,
-                    tax: 0.00
-                }
-            ]
-        }
+    @api recordId;
+    @track totalColumns;
+    @track columns;
+    @track quoteLines;
+    @track data = [];
+    @track totalColumns;
+    @track rotationClass = '';
+    @track grandTotalList = [];
+    @track filterModal = false;
+    @track filterValue ='PriceBook' ;
+    @track filterOption =[
+        {label: 'PriceBook', value: 'PriceBook'},
+        {label: 'Vendor', value: 'Vendor'},
     ];
+    @track filterGroupId;
+    @track showPricebookModal = false;
 
-    columns = [
-        {
-            type: 'action',
-            typeAttributes: { rowActions: this.getRowActions },
-            fixedWidth: 75
-        },
-        { label: 'Product Name', fieldName: 'productName' },
-        { label: 'Cost Code', fieldName: 'costCode' },
-        { label: 'Quantity', fieldName: 'quantity', type: 'number' },
-        { label: 'Unit Cost', fieldName: 'unitCost', type: 'currency' },
-        { label: 'Markup (%)', fieldName: 'markup', type: 'percent' },
-        { label: 'Unit Sales Price', fieldName: 'unitSalesPrice', type: 'currency' },
-        { label: 'Total Pre-Tax', fieldName: 'totalPreTax', type: 'currency' },
-        { label: 'Tax', fieldName: 'tax', type: 'currency' }
-    ];
-        renderedCallback() {
+    connectedCallback(){
+        this.getData();
+    }
+
+    renderedCallback() {
         if (this.isInitalRender) {
             const body = document.querySelector("body");
 
@@ -63,36 +35,166 @@ export default class NewQuoteItemcmp extends LightningElement {
                     background: #e0ebfa !important;
                     color:#0176d3;
                 }
+
+                .lastRowCSS table tr:last-child {
+                    font-weight: 700;
+                }
             `;
 
             body.appendChild(style);
+
             this.isInitalRender = false;
         }
 
     }
 
-    getRowActions(row, doneCallback) {
-        const actions = [
-            { label: 'Edit', name: 'edit', iconName: 'utility:edit' },
-            { label: 'Delete', name: 'delete', iconName: 'utility:delete' }
-        ];
-        doneCallback(actions);
+    getData(){
+        var QuoteId = this.recordId;
+        console.log('Recrod Id for the current quote is: ' + QuoteId);
+        getallData({ quoteId: QuoteId })
+            .then(result => {
+                console.log({result});
+
+                //loop on the colums cooming from FieldSet 
+                for(var i = 0; i < result.columns.length; i++){
+                    if(result.columns[i].label === 'Cost Code'){
+                        result.columns[i].fieldName = 'CostCode';
+                        result.columns[i].type = 'string';
+                    }
+
+                    result.columns[i].editable = false;
+                    result.columns[i].hideDefaultActions = true; 
+                    result.columns[i].cellAttributes = { alignment: 'left' };
+
+                    if(result.columns[i].fieldName == 'buildertek__Notes__c'){
+                        result.columns[i].wrapText = false;
+                    }else{
+                        result.columns[i].wrapText = true;
+                    }
+                    
+                    if(result.columns[i].label == 'Notes'){
+                        result.columns[i].initialWidth = 200;
+                    }
+                    else{
+                        result.columns[i].initialWidth = result.columns[i].label.length * 15;
+                    }
+
+
+                } 
+               
+                result.columns.unshift({
+                    // label: 'No.',
+                    fieldName: 'Number',
+                    type: 'string',
+                    editable: false,
+                    initialWidth: 80,
+                    hideDefaultActions: true,
+                    cellAttributes: { alignment: 'center' },
+                });
+                this.columns = result.columns;
+                this.totalColumns = result.columns;
+                this.quoteLines = result.quoteLineList;
+
+                //loop on the quote lines and group them by Grouping
+                for(var i = 0; i < this.quoteLines.length; i++){
+                    var groupName = this.quoteLines[i].buildertek__Grouping__r.Name;
+                    var groupId = this.quoteLines[i].buildertek__Grouping__c;
+                    if(this.quoteLines[i].buildertek__Cost_Code__c != null){
+                        this.quoteLines[i].CostCode = this.quoteLines[i].buildertek__Cost_Code__r.Name;
+                    }
+
+                    if(this.data.some(item => item.groupName === groupName && item.groupId === groupId)) {
+                        this.data.filter(item => item.groupName === groupName && item.groupId === groupId)[0].items.push(this.quoteLines[i]);
+                    } else {
+                        this.data.push({groupName: groupName, groupId: groupId, items: [this.quoteLines[i]]});
+                    }
+                    this.quoteLines[i].Number = i+1;
+                }
+                console.log({data: this.data});
+                this.calculateTotal(this.data);
+            })
+            .catch(error => {
+                console.log(error);
+            });
     }
 
-    groupSubtotal(items) {
-        return items.reduce((acc, item) => acc + item.totalPreTax, 0);
-    }
+    calculateTotal(data) {
+        let columns = this.columns;
+        let totalColumns = columns.filter(col => col.type === 'number' || col.type === 'currency');
+        let grandTotal = {};
 
-    get grandTotal() {
-        return this.data.reduce((acc, group) => acc + this.groupSubtotal(group.items), 0);
-    }
-
-    handleAddItem() {
-        const evt = new ShowToastEvent({
-            title: 'Add Item',
-            message: 'Add Item button clicked!',
-            variant: 'info'
+        data.forEach(item => {
+            let subtotalList = [];
+            let subTotal = {};
+            totalColumns.forEach(col => {
+                subTotal[col.fieldName] = item.items.reduce((acc, currentItem) => acc + currentItem[col.fieldName], 0);
+                if (grandTotal[col.fieldName]) {
+                    grandTotal[col.fieldName] += subTotal[col.fieldName];
+                } else {
+                    grandTotal[col.fieldName] = subTotal[col.fieldName];
+                }
+            });
+            subTotal['Name'] = 'Subtotal';
+            item.items.push(subTotal);
+            subtotalList.push(subTotal);
+            item.subtotal = subtotalList;
+            item.isVisible = true;
         });
-        this.dispatchEvent(evt);
+
+        this.grandTotalList = [JSON.parse(JSON.stringify(grandTotal))];
+
+        console.log({ grandTotal });
+        console.log({ grandTotalList: this.grandTotalList });
+        console.log({ data });
     }
+
+    handleAddItem(event) {
+        var groupId = event.target.dataset.id;
+        console.log('Add Item button clicked for Group Id: ' + groupId);
+        if(groupId){
+            this.filterModal = true;
+            this.filterGroupId = groupId;
+        }else{
+            const evt = new ShowToastEvent({
+                title: 'Error',
+                message: 'Please select a group to add item',
+                variant: 'error'
+            });
+            this.dispatchEvent(evt);
+        }
+
+    }
+
+    applyFilter(){
+        var filterValue = this.filterValue;
+        console.log("User choose filter value: " + filterValue);
+        if(filterValue === 'PriceBook'){
+            this.filterModal = false;
+            var groupId = this.filterGroupId;
+            console.log('Group Id: ' + groupId);
+            this.showPricebookModal = true
+        }else if(filterValue === 'Vendor'){
+            this.filterModal = false;
+            this.showVendorModal = true;
+        }
+    }
+
+    filterChange(event){
+        this.filterValue = event.detail.value;
+    }
+
+    hideModalBox(){
+        this.filterModal = false;
+    }
+
+    dropdownHandler(event){
+        var groupId = event.target.dataset.id;
+        var data = this.data;
+        for(var i = 0; i < data.length; i++){
+            if(data[i].groupId === groupId){
+                data[i].isVisible = !data[i].isVisible;
+            }
+        }
+    }
+
 }
