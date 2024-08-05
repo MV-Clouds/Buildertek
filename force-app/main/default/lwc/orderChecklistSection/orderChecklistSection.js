4c69d9758e5bbdc5c870eac029b28039c75dbfea
Nishit <nishitsuthar123@gmail.com>
2024-08-05 17:17:21 +0530

    IBT-1804 5:15 pm monday evening subsection backup

--- force-app/main/default/lwc/orderChecklistSection/orderChecklistSection.js
+++ force-app/main/default/lwc/orderChecklistSection/orderChecklistSection.js
@@ -1,6 +1,8 @@
 import { LightningElement, api, track } from 'lwc';
 import getChecklistSectionSubsection from '@salesforce/apex/OrderChecklistController.getChecklistSectionSubsection';
 import saveSectionOrderOnServer from '@salesforce/apex/OrderChecklistController.updateCheckListForSectionOrder';
+import saveSubsectionOrderOnServer from '@salesforce/apex/OrderChecklistController.updateCheckListForSubsectionOrder';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class OrderChecklistSection extends LightningElement {
     @api checkListId;
@@ -9,50 +11,82 @@ export default class OrderChecklistSection extends LightningElement {
     @track comboOptions = [];
     comboValue = '';
     @track orderedSectionList = [];
-    @track orderedSubectionList = [];
-    @track jsonStringForSection; 
-    @track jsonStringForSubsection; 
+    @track orderedSubsectionList = [];
+    tempArrayToAppendDataForSubSection = [];
+    @track jsonStringForSection;
+    @track jsonStringForSubsection;
     @api isOrderSection = false;
     globalSubsectionMap = {};
-    
+
 
     connectedCallback() {
-        console.log('this is checklist id ',this.checkListId);
+        console.log('this is checklist id ', this.checkListId);
         this.fetchCheckListSections();
     }
 
-    fetchCheckListSections() {
-        getChecklistSectionSubsection({checkListId: this.checkListId})
-            .then(result => {
-                console.log('this is result ',result);
-                let orderedSection = result.storedSectionOrder ? JSON.parse(result.storedSectionOrder) : {};
-                let orderedSubsection = result.storedSubsectionOrder ? JSON.parse(result.storedSubsectionOrder) : {};
-                let tempOptionList = [];
-                console.log('ordered section ',orderedSection);
-                result.sectionList.forEach((section, index) => {
-                    section['label'] = section.Name;
-                    section['value'] = section.Name;
+    async fetchCheckListSections() {
+        let result = await getChecklistSectionSubsection({ checkListId: this.checkListId });
+        console.log('this is result ', result);
+        if (result.message === 'success' && this.isOrderSection) {
+            this.doOperationForSection(result);
+        } else if (result.message === 'success' && !this.isOrderSection) {
+            this.doOperationForSubsection(result);
+        } else {
+            this.showToast('Error', result.message, 'error');
+        }
+    }
 
-                    if(orderedSection[section.Name]) {
-                        this.orderedSectionList.push(section.Name);
-                    }
-                    if(section.Id in result?.subSectionMap){
-                        tempOptionList.push({'label': section.Name, 'value': section.Id});
-                    }
-                    // WARN: Adding lable and value for combobox values
-                    result.subSectionMap[section.Id].forEach((subSection, index) => {
-                        subSection['label'] = subSection.Name;
-                        subSection['value'] = subSection.Id;
-                    });
-                });
-                this.presentSections = result.sectionList;
-                this.comboOptions = [...tempOptionList];
-                this.globalSubsectionMap = result.subSectionMap;
-                console.log('combo options ',JSON.parse(JSON.stringify(this.comboOptions)));
-            })
-            .catch(error => {
-                console.log('this is error ',error);
+    doOperationForSection(result) {
+        let orderedSection = result.storedSectionOrder ? JSON.parse(result.storedSectionOrder) : {};
+        console.log('ordered section ', orderedSection);
+        result.sectionList.forEach((section, index) => {
+            //* create data for the lightning-dual-listbox
+            section['label'] = section.Name;
+            section['value'] = section.Name;
+
+            //* push data into list for value attribute in lightning-dual-listbox
+            if (orderedSection[section.Name]) {
+                this.orderedSectionList.push(section.Name);
+            }
+        });
+        this.presentSections = result.sectionList;
+    }
+
+    doOperationForSubsection(result) {
+        let orderedSubsection = result.storedSubsectionOrder ? JSON.parse(result.storedSubsectionOrder) : {};
+        console.log('orderedSubsection : ', orderedSubsection);
+        console.log('OUTPUT of subSectionMap : ', result.subSectionMap);
+
+        //* first create data for comboBox 
+        this.comboOptions = this.getValuesForComboBox(result.sectionList, result.subSectionMap);
+
+        //* add labels and values in subsection map
+        this.globalSubsectionMap = this.addLableAndValues(result.subSectionMap);
+    }
+
+    addLableAndValues(subSectionMap) {
+        let tempMap = {};
+        Object.keys(subSectionMap).forEach((key) => {
+            tempMap[key] = [];
+            subSectionMap[key].forEach((subSection) => {
+                subSection['label'] = subSection.Name;
+                subSection['value'] = subSection.Name;
+                // subSection['value'] = subSection.Id;
+                tempMap[key].push(subSection);
             });
+        });
+        return tempMap;
+    }
+
+    getValuesForComboBox(sectionList, subSectionMap) {
+        let tempOptionList = [];
+        sectionList.forEach((section) => {
+            // Check if the section has subsections in the map
+            if (subSectionMap[section.Id] && subSectionMap[section.Id].length > 0) {
+                tempOptionList.push({ 'label': section.Name, 'value': section.Id });
+            }
+        });
+        return tempOptionList;
     }
 
     cancelPopup() {
@@ -60,11 +94,16 @@ export default class OrderChecklistSection extends LightningElement {
         this.dispatchEvent(valueChangeEvent);
     }
 
-    handleChangeInCombo(event){
+    handleChangeInCombo(event) {
         console.log('this is handleChange method for comobo');
         this.comboValue = event.detail.value;
+        console.log('check presentSubsections before ',JSON.parse(JSON.stringify(this.presentSubsections)));
+        
         this.presentSubsections = this.globalSubsectionMap[this.comboValue];
-        console.log('this is sub section list ',JSON.parse(JSON.stringify(this.presentSubsections)));
+        let arrayOfSelectedSubsectionValuePerSectionId = [...this.tempArrayToAppendDataForSubSection]
+        this.orderedSubsectionList = arrayOfSelectedSubsectionValuePerSectionId.filter(item => item[this.comboValue] !== undefined).map(item => item[this.comboValue]);
+        console.log('this is orderedSubsectionList ', JSON.parse(JSON.stringify(this.orderedSubsectionList)));
+        console.log('this is sub section list ', JSON.parse(JSON.stringify(this.presentSubsections)));
     }
 
     handleOrderChange(event) {
@@ -78,24 +117,76 @@ export default class OrderChecklistSection extends LightningElement {
     }
 
     handleSubOrderChange(event) {
-        this.orderedSubectionList = event.target.value;
-        let orderedSubsectionJson = {};
-        this.orderedSectionList.forEach((value, key) => {
-            orderedSubsectionJson[value] = [];
-        });
+        this.orderedSubsectionList = event.target.value;
+        console.log('OUTPUT of orderedSubsectionList: ', JSON.parse(JSON.stringify(this.orderedSubsectionList)));
+        console.log('OUTPUT of comboValue: ', JSON.parse(JSON.stringify(this.comboValue)));
+        try {
+            let selectedComboValue = this.comboValue;
+            let orderedSubsectionJson = {};
+            let isExistingValue = false;
+            console.log('orderedSubsectionJson ', orderedSubsectionJson);
+            orderedSubsectionJson[selectedComboValue] = [];
 
-        this.jsonStringForSubsection = JSON.stringify(orderedSubsectionJson);
+            this.orderedSubsectionList.forEach((value, key) => {
+                orderedSubsectionJson[selectedComboValue]?.push(value);
+            });
+            this.tempArrayToAppendDataForSubSection.forEach((element, key) => {
+                if (element[selectedComboValue]) {
+                    this.tempArrayToAppendDataForSubSection.splice(key, 1);
+                }
+            });
+
+            this.tempArrayToAppendDataForSubSection.push(orderedSubsectionJson);
+            this.jsonStringForSubsection = JSON.stringify(this.tempArrayToAppendDataForSubSection);
+            console.log('chekc your output : ', JSON.parse(JSON.stringify(this.tempArrayToAppendDataForSubSection)));
+        } catch (error) {
+            console.log('OUTPUT : ', error);
+        }
     }
 
     saveOrder() {
-        saveSectionOrderOnServer({checkListId: this.checkListId, jsonSectionOrderedString: this.jsonStringForSection})
-            .then(result => {
-                console.log('this is result ',result);
-                this.cancelPopup();
-            })
-            .catch(error => {
-                console.log('this is error ',error);
-            });
+        console.log('orderedSectionList ', JSON.parse(JSON.stringify(this.orderedSectionList)));
+
+        if (this.isOrderSection) {
+            saveSectionOrderOnServer({ checkListId: this.checkListId, jsonSectionOrderedString: this.jsonStringForSection })
+                .then(this.cancelPopup())
+                .catch(error => {
+                    let { errorMessage, errorObject } = this.returnErrorMsg(error);
+                    this.showToast('Error', errorMessage, 'error');
+                });
+        } else {
+            saveSubsectionOrderOnServer({ checkListId: this.checkListId, jsonSubsectionOrderedString: this.jsonStringForSubsection })
+                .then(this.cancelPopup())
+                .catch(error => {
+                    let { errorMessage, errorObject } = this.returnErrorMsg(error);
+                    this.showToast('Error', errorMessage, 'error');
+                });
+        }
     }
 
+    returnErrorMsg(error) {
+        console.error('An error occurred:', error);
+
+        let errorMessage = 'Unknown error';
+        if (error && error.body) {
+            if (error.body.message) {
+                errorMessage = error.body.message;
+            } else if (error.body.pageErrors && error.body.pageErrors.length > 0) {
+                errorMessage = error.body.pageErrors[0].message;
+            }
+        } else if (error && error.message) {
+            errorMessage = error.message;
+        }
+
+        return { errorMessage, errorObject: error };
+    }
+
+    showToast(title, message, variant) {
+        const event = new ShowToastEvent({
+            title: title,
+            message: message,
+            variant: variant
+        });
+        this.dispatchEvent(event);
+    }
 }
